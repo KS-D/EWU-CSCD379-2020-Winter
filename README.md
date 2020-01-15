3a4c1b07877f8b7398cbc495c79b5139cbd429ef
Michael Stokesbary <mike@stokesbary.me>
2020-01-15 11:03:38 -0800

    Configured a starting point for Assignment2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# CSCD-379-2020-Winter
+# Assignment3
 
-## See [Wiki](../../wiki)
+[![Build Status](https://dev.azure.com/KS-D/EWU-CSCD379-2020-Winter/_apis/build/status/KS-D.EWU-CSCD379-2020-Winter?branchName=Assignment3)](https://dev.azure.com/KS-D/EWU-CSCD379-2020-Winter/_build/latest?definitionId=3&branchName=Assignment3)
 
-[![Build Status](https://dev.azure.com/KS-D/EWU-CSCD379-2020-Winter/_apis/build/status/KS-D.EWU-CSCD379-2020-Winter?branchName=master)](https://dev.azure.com/KS-D/EWU-CSCD379-2020-Winter/_build/latest?definitionId=3&branchName=master)
+See [Assignment3.md](Assignment3.md)
--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -9,12 +9,16 @@ namespace SecretSanta.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+<<<<<<< HEAD
         //public void ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+=======
+        //public void ConfigureServices(IServiceCollection services)
+>>>>>>> Configured a starting point for Assignment2
         //{
         //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -25,10 +29,10 @@ namespace SecretSanta.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from API!");
-                });
+                _ = endpoints.MapGet("/", async context =>
+                  {
+                      await context.Response.WriteAsync("Hello from API!");
+                  });
             });
         }
     }
