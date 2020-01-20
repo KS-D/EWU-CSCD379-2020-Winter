1fa5cd5ea8d5e4dbb7dc2518685872f4c460997c
Kyle Smith <tikioph@gmail.com>
2020-01-19 21:17:27 -0800

    added another test to groupusertests, and fixed some formatting

--- SecretSanta/test/SecretSanta.Data.Tests/GroupTests.cs
+++ SecretSanta/test/SecretSanta.Data.Tests/GroupTests.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace SecretSanta.Data.Tests
 {
@@ -17,8 +20,8 @@ namespace SecretSanta.Data.Tests
 
         [TestMethod]
         public async Task Create_Group_DatabaseShouldStoreGroup()
-        {
-             var groupId = -1;
+        { 
+            var groupId = -1;
 
             using (var applicationDbContext = new ApplicationDbContext(Options))
             {
@@ -34,9 +37,79 @@ namespace SecretSanta.Data.Tests
                 var group = await applicationDbContext.Groups.Where(u => u.Id == groupId).SingleOrDefaultAsync();
                 
                 Assert.IsNotNull(group);
-                Assert.AreEqual("TheGroup", group.Name);
+                Assert.AreEqual(_Group.Name, group.Name);
+            }
+        } 
+        
+        [TestMethod]
+        public async Task Create_Group_ShouldSetFingerPrintOnInitialSave()
+        {
+            var httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
+                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, "kyle"));
+
+            var groupId = -1;
+
+            using (var applicationDbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                applicationDbContext.Groups.Add(_Group);
+                await applicationDbContext.SaveChangesAsync();
+
+                groupId = _Group.Id;
+            }
+            
+            using (var applicationDbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var group = await applicationDbContext.Groups.Where(g => g.Id == groupId).SingleOrDefaultAsync();
+                
+                Assert.IsNotNull(group);
+                Assert.AreEqual("kyle", group.CreatedBy);
+                Assert.AreEqual("kyle", group.ModifiedBy);
+            }
+        }
+
+        [TestMethod] 
+        public async Task Create_Group_ShouldSetFingerPrintOnUpdateSave()
+        {
+            const string user = "kyle";
+            var httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
+                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, user));
+
+            var groupId = -1;
+
+            using (var applicationDbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                applicationDbContext.Groups.Add(_Group);
+                await applicationDbContext.SaveChangesAsync();
+
+                groupId = _Group.Id;
             }
 
+            const string updatedUser = "TotallyNotKyle";
+            const string updatedName = "a new group name";
+
+            httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
+                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, updatedUser));
+            
+            using (var applicationDbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var group = await applicationDbContext.Groups.Where(g => g.Id == groupId).SingleOrDefaultAsync();
+                group.Name = updatedName;
+
+                await applicationDbContext.SaveChangesAsync();
+            }
+            
+            using (var applicationDbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var group = await applicationDbContext.Groups.Where(g => g.Id == groupId).SingleOrDefaultAsync();
+                
+                Assert.IsNotNull(group);
+                Assert.AreEqual(updatedName, group.Name);
+                Assert.AreEqual(user, group.CreatedBy);
+                Assert.AreEqual(updatedUser, group.ModifiedBy);
+                Assert.IsTrue(group.ModifiedOn > group.CreatedOn);
+
+            }
         }
+
     }
 }
--- SecretSanta/test/SecretSanta.Data.Tests/GroupUserTests.cs
+++ SecretSanta/test/SecretSanta.Data.Tests/GroupUserTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,63 +13,114 @@ namespace SecretSanta.Data.Tests
     [TestClass]
     public class GroupUserTests : TestBase
     {
+        private readonly User _User = new User
+            {
+                FirstName = "kyle",
+                LastName = "Smith",
+                GroupUsers = new List<GroupUser>()
+            };
+
+        private readonly Gift _Gift = new Gift
+            {
+                Title = "Gift",
+                Description = "a nice gift",
+                Url = "www.gifts.com"
+            };
+
+        private readonly Group _Group1 = new Group { Name = "A group" };
+        private readonly Group  _Group2 = new Group { Name = "Another group" };
+ 
         [TestMethod]
         public async Task Create_GroupUser_WithMultipleGroups()
         {
-           IHttpContextAccessor httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
+            IHttpContextAccessor httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, "kyle"));
+
+            var groupUser1 = new GroupUser { User = _User, Group = _Group1 };
+            var groupUser2 = new GroupUser { User = _User, Group = _Group2 };
+
+            var groupUsers = new List<GroupUser>{ groupUser1, groupUser2 };
+            
+            _Gift.User = _User;
+            _User.Gifts = new List<Gift>{ _Gift };
+            _User.GroupUsers = groupUsers;
            
-           var user = new User
-           {
-               FirstName = "kyle",
-               LastName = "Smith",
-           };
+            using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            { 
+                dbContext.Users.Add(_User);
+                await dbContext.SaveChangesAsync();
+            }
 
-           var gift = new Gift
-           {
-               Title = "Gift",
-               Description = "a nice gift",
-               Url = "www.gifts.com"
-           };
 
-           var group1 = new Group { Name = "A group" };
-           var group2 = new Group { Name = "Another group" };
+            using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            { 
+                var userFromDb = await dbContext.Users.Where(u => u.Id == _User.Id).Include(u => u.Gifts)
+                    .Include(u => u.GroupUsers).ThenInclude(g => g.Group).SingleOrDefaultAsync();
 
-           var groupUser1 = new GroupUser { User = user, Group = group1 };
-           var groupUser2 = new GroupUser { User = user, Group = group2 };
+                var giftOnUser = userFromDb.Gifts.ElementAt(0);
+                    
+                Assert.IsNotNull(userFromDb);
+                Assert.AreEqual(_User.FirstName, userFromDb.FirstName);
+                Assert.AreEqual(_User.LastName, userFromDb.LastName);
+                Assert.AreEqual(_User.Id, userFromDb.Id);
+                Assert.IsNotNull(_User.GroupUsers[0]);
+                Assert.IsNotNull(_User.GroupUsers[1]);
+                Assert.AreEqual(groupUsers[0].GroupId, userFromDb.GroupUsers[0].GroupId);
+                Assert.AreEqual(groupUsers[0].UserId, userFromDb.GroupUsers[0].UserId);
+                Assert.AreEqual(groupUsers[1].GroupId, userFromDb.GroupUsers[1].GroupId);
+                Assert.AreEqual(groupUsers[1].UserId, userFromDb.GroupUsers[1].UserId);
+                Assert.AreEqual(1, userFromDb.Gifts.Count);
+                Assert.AreEqual(_Gift.Url, giftOnUser.Url);
+                Assert.AreEqual(_Gift.Title, giftOnUser.Title);
+                Assert.AreEqual(_Gift.Description, giftOnUser.Description);
+            }
+        }
 
-           var groupUsers = new List<GroupUser>{ groupUser1, groupUser2 };
+        [TestMethod]
+        public async Task Create_GroupUser_WithMultipleUsers()
+        {
+            IHttpContextAccessor httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta => 
+               hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, "kyle"));
+
+            var user2 = new User
+            {
+                FirstName = "two",
+                LastName = "Two",
+                Gifts = new List<Gift>()
+            };
+            
+            var groupUser1 = new GroupUser { User = _User, Group = _Group1 };
+            var groupUser2 = new GroupUser {User = user2, Group = _Group1 };
+
+            var groupUsers = new List<GroupUser>{ groupUser1 };
+            var groupUsers2 = new List<GroupUser> { groupUser2 };
+            
+            _Gift.User = _User;
+            _User.Gifts = new List<Gift>{ _Gift };
+            _User.GroupUsers = groupUsers;
+            user2.GroupUsers = groupUsers2;
            
-           gift.User = user;
-           user.Gifts = new List<Gift>{ gift };
-           user.GroupUsers = groupUsers;
-           
-           using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
-           {
-               dbContext.Users.Add(user);
-               await dbContext.SaveChangesAsync();
-           }
+            using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                dbContext.Users.Add(_User);
+                dbContext.Users.Add(user2);
+                await dbContext.SaveChangesAsync();
+            }
 
+            using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var groupsFromDb = await dbContext.Groups.Where(g => g.Id == _Group1.Id)
+                                                  .Include(u => u.GroupUsers)
+                                                  .ThenInclude(u => u.User).SingleOrDefaultAsync();
 
-           using (ApplicationDbContext dbContext = new ApplicationDbContext(Options, httpContextAccessor))
-           {
-               var UserFromDb = await dbContext.Users.Where(u => u.Id == user.Id).Include(u => u.Gifts)
-                   .Include(u => u.GroupUsers).ThenInclude(g => g.Group).SingleOrDefaultAsync();
+                Assert.IsNotNull(groupsFromDb);
+                Assert.AreEqual(_Group1.Id, groupsFromDb.Id);
+                Assert.IsNotNull(groupsFromDb.GroupUsers);
+                Assert.AreEqual(2, groupsFromDb.GroupUsers.Count);
+                Assert.AreEqual(_User.Id, groupsFromDb.GroupUsers[0].UserId);
+                Assert.AreEqual(user2.Id, groupsFromDb.GroupUsers[1].UserId);
+            }
 
-               var giftOnUser = UserFromDb.Gifts.ElementAt(0);
-
-               Assert.IsNotNull(UserFromDb);
-               Assert.AreEqual(user.FirstName, UserFromDb.FirstName);
-               Assert.AreEqual(user.LastName, UserFromDb.LastName);
-               Assert.AreEqual(1, UserFromDb.Gifts.Count);
-               Assert.AreEqual(user.Id, UserFromDb.Id);
-               Assert.AreEqual(gift.Url, giftOnUser.Url);
-               Assert.AreEqual(gift.Title, giftOnUser.Title);
-               Assert.AreEqual(gift.Description, giftOnUser.Description);
-               Assert.IsNotNull(user.GroupUsers[0]);
-               Assert.IsNotNull(user.GroupUsers[1]);
-               CollectionAssert.AreEqual(user.GroupUsers, gift.User.GroupUsers);
-           }
         }
     }
 }
