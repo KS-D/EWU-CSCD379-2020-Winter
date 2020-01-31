ea760b58d425491ccd05b8e3dfd76ce0dfbe8796
Kyle Smith <tikioph@gmail.com>
2020-01-30 21:30:21 -0800

    started writing controller tests

---
+++ SecretSanta/test/SecretSanta.Api.Tests/Controller/UserControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Business;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
+
+namespace SecretSanta.Api.Tests.Controller
+{
+    [TestClass]
+    public class UserControllerTests 
+    {
+
+        [TestMethod]
+        public void Create_UserController_Success()
+        {
+            
+            var userController = new UserController(new Mock<IUserService>().Object);
+
+            Assert.IsNotNull(userController);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Create_PassedNullService_ThrowsError()
+        {
+            var userController = new UserController(null!);
+        }
+
+        [TestMethod]
+        public async Task Get_EntitiesInDatabase_ReturnListOfEntities()
+        {
+            var service = new Mock<IUserService>();
+            service.Setup(service => service.FetchAllAsync())
+                .Returns(Task.FromResult(new List<User>{SampleData.CreateUserInigo(), SampleData.CreateUserButtercup()}));
+            var userController = new UserController(service.Object);
+
+            List<User> users = (List<User>)await userController.Get();
+
+            Assert.IsNotNull(users);
+            Assert.AreEqual((SampleData.Inigo, SampleData.Montoya), (users[0].FirstName, users[0].LastName));
+            Assert.AreEqual((SampleData.Princess, SampleData.Buttercup), (users[1].FirstName, users[1].LastName));
+        }
+
+        [TestMethod]
+        public async Task Get_ItemById_Success()
+        {
+            var service = new Mock<IUserService>();
+            service.Setup(service => service.FetchByIdAsync(42))
+                .Returns(Task.FromResult(SampleData.CreateUserInigo()));
+            var userController = new UserController(service.Object);
+
+            ActionResult<User> inigoResult = await userController.Get(42);
+
+            Assert.IsInstanceOfType(inigoResult.Result, typeof(OkObjectResult));
+            OkObjectResult ok = (OkObjectResult)inigoResult.Result;
+            User inigo = (User)ok.Value;
+            Assert.IsNotNull(inigo);
+            Assert.AreEqual((SampleData.Inigo, SampleData.Montoya), (inigo.FirstName, inigo.LastName));
+            Assert.AreEqual((SampleData.Inigo, SampleData.Montoya), (inigo.FirstName, inigo.LastName));
+        }
+
+        [TestMethod]
+        public void Post_UserController_Success()
+        {
+            var userController = new UserController(new Mock<IUserService>().Object);
+        }
+    }
+}
