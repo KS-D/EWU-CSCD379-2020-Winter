4000edfe26960d34fd7c0883c7ca8bb1033a2aad
Kyle Smith <tikioph@gmail.com>
2020-02-07 21:20:50 -0800

    Swagger crud endpoints work

--- SecretSanta/src/SecretSanta.Business/Dto/GroupInput.cs
+++ SecretSanta/src/SecretSanta.Business/Dto/GroupInput.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SecretSanta.Business.Dto
 {
     public class GroupInput
     {
+        [Required]
         public string? Title { get; set; }
 
     }
