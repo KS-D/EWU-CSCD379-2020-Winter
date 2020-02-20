763a76772e65b2d67afc2b50022aa19865b273d7
Kyle Smith <tikioph@gmail.com>
2020-02-19 20:23:30 -0800

    tried to write the tests, but there is some issue running them

--- SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.spec.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.spec.ts
@@ -1,1 +1,21 @@
-﻿
+﻿import { ListGift } from "./list-Gifts"
+
+import { expect } from "chai"
+
+import 'mocha';
+
+describe('GetAllGifts', () => {
+    it('should return all gifts', async () =>{
+        const display = new ListGift();
+        const actual = await display.getAllGifts();
+        expect(actual.length).to.equal(0);
+    })
+});
+
+describe('fakeGiftLists', () => {
+    it('should return all gifts', async () =>{
+        const display = new ListGift();
+        const actual = await display.fakeGiftLists();
+        expect(actual.length).to.equal(5);
+    })
+})
--- SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
@@ -1,1 +1,58 @@
-﻿
+﻿import { GiftClient, Gift, GiftInput } from "./secretsanta-client"
+
+export class ListGift
+{
+    async getAllGifts(): Promise<Gift[]> {
+        var client = new GiftClient();
+        var gifts = await client.getAll();
+        return gifts;
+    }
+
+    async fakeGiftLists(): Promise<Gift[]> {
+        var client = new GiftClient();
+        var gifts = await this.getAllGifts();
+        (gifts).forEach( gift => {
+            client.delete(gift.id);
+        });
+        let fakeGifts : Gift[] = [
+                                    new Gift({
+                                                id: 1, 
+                                                title: "Princess Bride: Movie", 
+                                                description: "pretty good", 
+                                                url: "www.movie.com", 
+                                                userId: 1
+                                            }), 
+                                    new Gift({
+                                                id: 2, 
+                                                title: "Princess Bride: Book", 
+                                                description: "pretty good", 
+                                                url: "www.book.com", 
+                                                userId: 1
+                                            }),
+                                    new Gift({
+                                                id: 3, 
+                                                title: "Six Fingered Glove", 
+                                                description: "It has six fingers", 
+                                                url: "www.specialgloves.com",
+                                                 userId: 1
+                                            }),
+                                    new Gift({
+                                                id: 4,
+                                                title: "Iocane powder",
+                                                description: "You can train yourself to" +
+                                                              "be immune to this poison",
+                                                url: "www.superdeadly.com",
+                                                userId: 1
+                                            }), 
+                                    new Gift({
+                                                id: 5,
+                                                title: "R.O.U.S.",
+                                                description: "It is a rodent of unusual size",
+                                                url: "www.fireswamp.com", 
+                                                userId: 1
+                                            })
+                                ];
+
+        return fakeGifts;
+    }
+}
