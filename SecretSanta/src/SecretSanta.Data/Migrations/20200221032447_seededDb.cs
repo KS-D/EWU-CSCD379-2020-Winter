ca5b2124dbf9fd572a591fbe8304ec7722a97ed3
Kyle Smith <tikioph@gmail.com>
2020-02-20 20:56:53 -0800

    add the rest of the files

---
+++ SecretSanta/src/SecretSanta.Data/Migrations/20200221032447_seededDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecretSanta.Data.Migrations
+{
+    public partial class seededDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedBy", "CreatedOn", "FirstName", "LastName", "ModifiedBy", "ModifiedOn", "SantaId" },
+                values: new object[] { 1, "Kyle Smith", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inigo", "Montoya", "Kyle Zmith", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
